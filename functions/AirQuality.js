df9c1ae7fc3deccc35cb4fa419dfd5bb0e71a3e3
moontai0724 <moontai0724@gmail.com>
2018-04-06 23:28:14 +0800

    更新即時觀測資訊

--- functions/AirQuality.js
+++ functions/AirQuality.js
@@ -11,82 +11,76 @@ const UTC8Time = require('./UTC8Time'); //ok.include: getNowTime(function), valu
 
 // Source: https://data.gov.tw/dataset/40448
 
-var item = ["AQI", "SO2", "CO", "CO_8hr", "O3", "O3_8hr", "PM10", "PM2.5", "NO2", "NOx", "NO", "WindSpeed", "WindDirec", "PM2.5_AVG", "PM10_AVG"],
-    lastTime = undefined,
-    result = undefined;
+var item = ["AQI", "SO2", "CO", "CO_8hr", "O3", "O3_8hr", "PM10", "PM2.5", "NO2", "NOx", "NO", "WindSpeed", "WindDirec", "PM2.5_AVG", "PM10_AVG"];
 
 module.exports = {
     get: async function (county) {
         return new Promise(resolve => {
-            let time = new Date().getDate + new Date().getHours;
-            if (lastTime != time) {
-                lastTime = time;
-                await $({
-                    mode: "GET",
-                    url: "http://opendata2.epa.gov.tw/AQI.json",
-                    success: function (data) {
-                        data = JSON.parse(data);
-                        result = {
-                            "County": 0,
-                            "AQI": 0,
-                            "Pollutant": "",
-                            "Status": "",
-                            "SO2": 0,
-                            "CO": 0,
-                            "CO_8hr": 0,
-                            "O3": 0,
-                            "O3_8hr": 0,
-                            "PM10": 0,
-                            "PM2.5": 0,
-                            "NO2": 0,
-                            "NOx": 0,
-                            "NO": 0,
-                            "WindSpeed": 0,
-                            "WindDirec": 0,
-                            "PM2.5_AVG": 0,
-                            "PM10_AVG": 0
+            $({
+                mode: "GET",
+                url: "http://opendata2.epa.gov.tw/AQI.json",
+                success: function (data) {
+                    data = JSON.parse(data);
+                    result = {
+                        "County": 0,
+                        "AQI": 0,
+                        "Pollutant": "",
+                        "Status": "",
+                        "SO2": 0,
+                        "CO": 0,
+                        "CO_8hr": 0,
+                        "O3": 0,
+                        "O3_8hr": 0,
+                        "PM10": 0,
+                        "PM2.5": 0,
+                        "NO2": 0,
+                        "NOx": 0,
+                        "NO": 0,
+                        "WindSpeed": 0,
+                        "WindDirec": 0,
+                        "PM2.5_AVG": 0,
+                        "PM10_AVG": 0
+                    }
+                    data.forEach(element => {
+                        if (element.County == county) {
+                            result.Status = element.Status;
+                            result.County = element.County;
+                            for (let i = 0; i < item.length; i++) {
+                                if (!isNaN(element[item[i]]))
+                                    result[item[i]] = (Number(result[item[i]]) + Number(element[item[i]])) / 2;
+                            }
                         }
-                        data.forEach(element => {
-                            if (element.County == county) {
-                                result.Status = element.Status;
-                                result.County = element.County;
-                                for (let i = 0; i < item.length; i++) {
-                                    if (!isNaN(element[item[i]]))
-                                        result[item[i]] = (Number(result[item[i]]) + Number(element[item[i]])) / 2;
-                                }
-                            }
-                        });
-                        for (let i = 0; i < item.length; i++) {
-                            switch (item[i]) {
-                                case 'SO2': case 'CO': case 'CO_8hr': case 'NO2': case 'NOx': case 'NO':
-                                    result[item[i]] = Math.round(result[item[i]] * 10) / 10;
-                                    break;
-                                default:
-                                    result[item[i]] = Math.round(result[item[i]]);
-                                    break;
-                            }
+                    });
+                    for (let i = 0; i < item.length; i++) {
+                        switch (item[i]) {
+                            case 'SO2': case 'CO': case 'CO_8hr': case 'NO2': case 'NOx': case 'NO':
+                                result[item[i]] = Math.round(result[item[i]] * 10) / 10;
+                                break;
+                            default:
+                                result[item[i]] = Math.round(result[item[i]]);
+                                break;
                         }
                     }
-                });
-            }
-            reply = '\n空氣品質指標 AQI: ';
-            if (0 <= result.AQI && result.AQI <= 50) reply += '良好 (' + result.AQI + ')';
-            else if (51 <= result.AQI && result.AQI <= 100) reply += '普通 (' + result.AQI + ')';
-            else if (101 <= result.AQI && result.AQI <= 199) reply += '不良 (' + result.AQI + ')';
-            else if (200 <= result.AQI && result.AQI <= 299) reply += '非常不良 (' + result.AQI + ')';
-            else if (300 <= result.AQI && result.AQI <= 500) reply += '有害 (' + result.AQI + ')';
-            reply += '\n細懸浮微粒 PM2.5: ';
-            if (0 <= result['PM2.5'] && result['PM2.5'] <= 11) reply += '1 低 (' + result['PM2.5'] + ')';
-            else if (12 <= result['PM2.5'] && result['PM2.5'] <= 23) reply += '2 低 (' + result['PM2.5'] + ')';
-            else if (24 <= result['PM2.5'] && result['PM2.5'] <= 35) reply += '3 低 (' + result['PM2.5'] + ')';
-            else if (36 <= result['PM2.5'] && result['PM2.5'] <= 41) reply += '4 中 (' + result['PM2.5'] + ')';
-            else if (42 <= result['PM2.5'] && result['PM2.5'] <= 47) reply += '5 中 (' + result['PM2.5'] + ')';
-            else if (48 <= result['PM2.5'] && result['PM2.5'] <= 53) reply += '6 中 (' + result['PM2.5'] + ')';
-            else if (54 <= result['PM2.5'] && result['PM2.5'] <= 58) reply += '7 高 (' + result['PM2.5'] + ')';
-            else if (59 <= result['PM2.5'] && result['PM2.5'] <= 64) reply += '8 高 (' + result['PM2.5'] + ')';
-            else if (65 <= result['PM2.5'] && result['PM2.5'] <= 70) reply += '9 高 (' + result['PM2.5'] + ')';
-            else if (71 <= result['PM2.5']) reply += '10 非常高 (' + result['PM2.5'] + ')';
-            resolve(reply);
+                    reply = '\n空氣品質指標 AQI: ';
+                    if (0 <= result.AQI && result.AQI <= 50) reply += '良好 (' + result.AQI + ')';
+                    else if (51 <= result.AQI && result.AQI <= 100) reply += '普通 (' + result.AQI + ')';
+                    else if (101 <= result.AQI && result.AQI <= 199) reply += '不良 (' + result.AQI + ')';
+                    else if (200 <= result.AQI && result.AQI <= 299) reply += '非常不良 (' + result.AQI + ')';
+                    else if (300 <= result.AQI && result.AQI <= 500) reply += '有害 (' + result.AQI + ')';
+                    reply += '\n細懸浮微粒 PM2.5: ';
+                    if (0 <= result['PM2.5'] && result['PM2.5'] <= 11) reply += '1 低 (' + result['PM2.5'] + ')';
+                    else if (12 <= result['PM2.5'] && result['PM2.5'] <= 23) reply += '2 低 (' + result['PM2.5'] + ')';
+                    else if (24 <= result['PM2.5'] && result['PM2.5'] <= 35) reply += '3 低 (' + result['PM2.5'] + ')';
+                    else if (36 <= result['PM2.5'] && result['PM2.5'] <= 41) reply += '4 中 (' + result['PM2.5'] + ')';
+                    else if (42 <= result['PM2.5'] && result['PM2.5'] <= 47) reply += '5 中 (' + result['PM2.5'] + ')';
+                    else if (48 <= result['PM2.5'] && result['PM2.5'] <= 53) reply += '6 中 (' + result['PM2.5'] + ')';
+                    else if (54 <= result['PM2.5'] && result['PM2.5'] <= 58) reply += '7 高 (' + result['PM2.5'] + ')';
+                    else if (59 <= result['PM2.5'] && result['PM2.5'] <= 64) reply += '8 高 (' + result['PM2.5'] + ')';
+                    else if (65 <= result['PM2.5'] && result['PM2.5'] <= 70) reply += '9 高 (' + result['PM2.5'] + ')';
+                    else if (71 <= result['PM2.5']) reply += '10 非常高 (' + result['PM2.5'] + ')';
+                    resolve(reply);
+                }
+            });
         });
     }
 }
--- functions/Ultraviolet.js
+++ functions/Ultraviolet.js
@@ -11,35 +11,30 @@ const UTC8Time = require('./UTC8Time'); //ok.include: getNowTime(function), valu
 
 // Source: https://data.gov.tw/dataset/6076
 
-var lastTime = undefined, result = undefined;
-
 module.exports = {
     get: function (county) {
         return new Promise(resolve => {
-            let time = new Date().getDate + new Date().getHours;
-            if (lastTime != time) {
-                $({
-                    mode: "GET",
-                    url: "http://opendata2.epa.gov.tw/UV/UV.json",
-                    success: function (data) {
-                        data = JSON.parse(data);
-                        let UVI = 0;
-                        data.forEach(element => {
-                            if (element.County == county)
-                                if (!isNaN(element.UVI))
-                                    UVI = (Number(UVI) + Number(element.UVI)) / 2;
-                        });
-                        UVI = Math.round(UVI);
-                    }
-                });
-            }
-            reply = '\n紫外線指數：';
-            if (0 <= UVI && UVI <= 2) reply += '􀔃􀇫small green triangle􏿿微量級 (' + UVI + '): 對於一般人無危險';
-            else if (3 <= UVI && UVI <= 5) reply += '􂜁􀆑.􏿿低量級 (' + UVI + '): 無保護暴露於陽光中有較輕傷害的風險';
-            else if (6 <= UVI && UVI <= 7) reply += '􀔃􀇪small yellow reversed triangle􏿿中量級 (' + UVI + '): 無保護暴露於陽光中有很大傷害的風險';
-            else if (8 <= UVI && UVI <= 10) reply += '􂘁􀆑.􏿿過量級 (' + UVI + '): 暴露於陽光中有極高風險';
-            else if (11 <= UVI) reply += '􂘁􀆋down􏿿危險級 (' + UVI + '): 暴露於陽光中極其危險';
-            resolve(reply);
+            $({
+                mode: "GET",
+                url: "http://opendata2.epa.gov.tw/UV/UV.json",
+                success: function (data) {
+                    data = JSON.parse(data);
+                    let UVI = 0;
+                    data.forEach(element => {
+                        if (element.County == county)
+                            if (!isNaN(element.UVI))
+                                UVI = (Number(UVI) + Number(element.UVI)) / 2;
+                    });
+                    UVI = Math.round(UVI);
+                    reply = '\n紫外線指數：';
+                    if (0 <= UVI && UVI <= 2) reply += '􀔃􀇫small green triangle􏿿微量級 (' + UVI + '): 對於一般人無危險';
+                    else if (3 <= UVI && UVI <= 5) reply += '􂜁􀆑.􏿿低量級 (' + UVI + '): 無保護暴露於陽光中有較輕傷害的風險';
+                    else if (6 <= UVI && UVI <= 7) reply += '􀔃􀇪small yellow reversed triangle􏿿中量級 (' + UVI + '): 無保護暴露於陽光中有很大傷害的風險';
+                    else if (8 <= UVI && UVI <= 10) reply += '􂘁􀆑.􏿿過量級 (' + UVI + '): 暴露於陽光中有極高風險';
+                    else if (11 <= UVI) reply += '􂘁􀆋down􏿿危險級 (' + UVI + '): 暴露於陽光中極其危險';
+                    resolve(reply);
+                }
+            });
         });
     }
 }
