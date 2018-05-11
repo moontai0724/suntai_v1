33e9f689adc0bb433b8790b92c04a3e139b63f82
moontai0724 <moontai0724@gmail.com>
2018-05-11 21:40:01 +0800

    TESTING

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ router.post('/', ctx => {
 		}
 	} else if (ctx.request.header['user-agent'].includes('GitHub')) {
 		console.log(Config.GitHub.webhookSecret);
+		console.log(ctx.request.header);
 		console.log(ctx.request.header['X-Hub-Signature']);
 		console.log('sha1=' + crypto.createHmac('SHA1', Config.GitHub.webhookSecret).update(ctx.request.rawBody).digest('hex'));
 		if (ctx.request.header['X-Hub-Signature'] == 'sha1=' + crypto.createHmac('SHA1', Config.GitHub.webhookSecret).update(ctx.request.rawBody).digest('hex')) {
