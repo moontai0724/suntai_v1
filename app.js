d45cd31471ff2bb284b34c1af1a33ac04e1fa25a
moontai0724 <moontai0724@gmail.com>
2018-05-11 20:30:30 +0800

    TESTING

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ router.post('/', ctx => {
 	console.log('ctx', JSON.stringify(ctx));
 	console.log('ctx.request', JSON.stringify(ctx.request));
 	console.log('ctx.request.header', JSON.stringify(ctx.request.header));
-	console.log('ctx.req', JSON.stringify(ctx.req));
-	console.log('ctx.res', JSON.stringify(ctx.res));
+	console.log('ctx.req', ctx.req);
+	console.log('ctx.res', ctx.req);
 	if (ctx.request.header['user-agent'].includes('LineBotWebhook')) {
 		const req = ctx.request;
 		if (LineBotSDK.validateSignature(req.rawBody, Config.LineBot.channelSecret, req.headers['x-line-signature'])) {
